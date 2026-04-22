8eaf424dce80b1939d7f3328a7c58a6d0b534380
Edson-dev146 <felix.e@escolar.ifrn.edu.br>
2026-04-22 17:14:09 -0300

    Update app.py
    esqueleto das rotas

--- app.py
+++ app.py
@@ -10,12 +10,40 @@ def index():
 
 @app.route('/cadastro')
 def cadastro():
-    return render_template ('cadastro.html')
+
+
+    nome= request.form.get('nome')
+    email=request.form.get('email')
+    senha=request.form.get('senha')
+    telefone=request.form.get('telefone')
+    endereco=request.form.get('endereco')
+    data_de_nascimento=request.form.get('data_de_nascimento')
+
+
+
+
+
+    return render_template ('cadastro.html',nome=nome,email=email,senha=senha,telefone=telefone,endereco=endereco,data_de_nascimento=data_de_nascimento)
 
 
 @app.route('/login')
 def login():
+
+
     return render_template('login.html')
+
+
+@app.route('/area_livros')
+def area_livros():
+
+
+    return render_template('area_livros.html')
+
+
+@app.route('/carrinho')
+def carrinho():
+    
+    return render_template('carrinho.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
