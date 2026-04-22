8effaa50c44eb08cda5d6dfd3db3ce8604e8004c
Edson-dev146 <felix.e@escolar.ifrn.edu.br>
2026-04-22 17:01:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,19 @@ from flask import Flask, render_template,request,redirect,url_for
 app = Flask(__name__)
 
 
-@app.route('/login')
-def cadastro():
+@app.route('/')
+def index():
     
-    return render_template('login.html')
+    return render_template('index.html')
 
+@app.route('/cadastro')
+def cadastro():
+    return render_template ('cadastro.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
