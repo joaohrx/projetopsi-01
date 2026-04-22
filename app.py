633dabfddeb3c3b017f606fdde88bd73afbb0cdf
Edson2025-dev <edsonfelix188@gmail.com>
2026-04-22 20:50:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template,request,redirect,url_for
 
 app = Flask(__name__)
 
+logado = False
+
 
 @app.route('/')
 def index():
@@ -27,19 +29,40 @@ def cadastro():
     return render_template('cadastro.html')
 
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    global logado
 
     if request.method == 'POST':
         nome = request.form.get('nome')
+        email= request.form.get('email')
         senha = request.form.get('senha')
 
-        if nome and senha:
+        if nome and email and senha:
+            logado = True
             return redirect(url_for('dashboard'))
 
         return redirect(url_for('login'))
 
     return render_template('login.html')
+
+
+
+@app.route('/dashboard')
+def dashboard():
+    if not logado:
+        return redirect(url_for('login'))
+
+    return render_template('dashboard.html')
+
+
+
+@app.route('/logout')
+def logout():
+    global logado
+    logado = False
+    return redirect(url_for('login'))
 
 
 @app.route('/area_livros')
